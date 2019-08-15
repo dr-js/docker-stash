52baf4c9646d1896c5411623e1cd9439812a0578
dr-js <dr@dr.run>
2019-08-15 15:05:36 +0800

    docker-stash@0.0.4-dev.3
    notable change:
    - package update

---
+++ source/docker-push.js
@@ -0,0 +1,25 @@
+const { runSync } = require('dr-js/library/node/system/Run')
+const { runMain } = require('dr-dev/library/main')
+const { fromRoot, COMMAND_DOCKER, loadTagCoreAsync } = require('./function')
+
+runMain(async (logger) => {
+  logger.padLog('push')
+
+  const { version: BUILD_VERSION } = require(fromRoot('package.json'))
+  const BUILD_REPO = require('./ubuntu1804/BUILD_REPO.json')
+
+  const tagList = [
+    await loadTagCoreAsync(fromRoot(__dirname, 'ubuntu1804/')),
+    `${BUILD_REPO}:${BUILD_VERSION}-1804-node`,
+    `${BUILD_REPO}:${BUILD_VERSION}-1804-bin`,
+    `${BUILD_REPO}:${BUILD_VERSION}-1804-full`,
+    `${BUILD_REPO}:${BUILD_VERSION}-1804-node-cn`,
+    `${BUILD_REPO}:${BUILD_VERSION}-1804-bin-cn`,
+    `${BUILD_REPO}:${BUILD_VERSION}-1804-full-cn`,
+  ]
+
+  for (const tag of tagList) {
+    logger.log(`push tag: ${tag}`)
+    runSync({ command: COMMAND_DOCKER, argList: [ 'push', tag ] })
+  }
+}, 'docker-push')
