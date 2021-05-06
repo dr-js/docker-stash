c1d8f87137d389ec906667d6e65653de899aa0f3
dr-js <dr@dr.run>
2021-05-06 22:38:30 +0800

    FIX: patch for git HTTP2 error

--- source/debian10/build-layer-script/3-3-bin-git.sh
+++ source/debian10/build-layer-script/3-3-bin-git.sh
@@ -2,6 +2,14 @@
 
 source ./0-1-base-apt.sh
 
+{ # patch for git HTTP2 error: `unable to access 'https://github.com/{user}/{repo}/': Failed sending HTTP2 data`
+  # check: https://superuser.com/questions/1642858/git-throws-fatal-unable-to-access-https-github-com-user-repo-git-failed-se
+  # and: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=987188
+  echo 'Package: libcurl3-gnutls'
+  echo 'Pin: version 7.64.*'
+  echo 'Pin-Priority: 1001'
+} > /etc/apt/preferences.d/git-curl-ci-patch
+
 apt-update
   # TODO: the size increase is big (~70MiB), consider trim git-core/perl?
   apt-install git
