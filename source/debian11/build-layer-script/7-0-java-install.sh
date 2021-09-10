2bf7ce1355570a16d7971ab0a3d9e77b2c092d43
dr-js <dr@dr.run>
2021-09-10 19:40:12 +0800

    CHG: debian11: use LTS `java@17`

--- source/debian11/build-layer-script/7-0-java-install.sh
+++ source/debian11/build-layer-script/7-0-java-install.sh
@@ -4,7 +4,7 @@ source ./0-1-base-apt.sh
 
 apt-update
   mkdir -p /usr/share/man/man1/ # fix update-alternatives doesn't respect path-exclude # https://github.com/debuerreotype/docker-debian-artifacts/issues/24
-  apt-install openjdk-11-jre-headless
+  apt-install openjdk-17-jre-headless
   rm -rf /usr/share/man/man1/
 apt-clear
 
