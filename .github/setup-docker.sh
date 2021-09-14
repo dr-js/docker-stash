6dc1383e867f49817c4333671a40ac70c620022a
dr-js <dr@dr.run>
2021-09-14 17:13:45 +0800

    CHG: ci: config update

---
+++ .github/setup-docker.sh
@@ -0,0 +1,11 @@
+# expect `cwd` at repo root
+
+# https://stackoverflow.com/questions/60171603/enable-experimental-features-on-github-workflow-images/60454218#60454218
+echo '{ "experimental": true, "features": { "buildkit": true } }' | sudo tee /etc/docker/daemon.json
+sudo systemctl restart docker
+
+echo '"drjs/debian"' > source/debian10/BUILD_REPO.json
+echo '"ghcr.io/dr-js/debian"' > source/debian10/BUILD_REPO_GHCR.json
+
+echo '"drjs/debian"' > source/debian11/BUILD_REPO.json
+echo '"ghcr.io/dr-js/debian"' > source/debian11/BUILD_REPO_GHCR.json
