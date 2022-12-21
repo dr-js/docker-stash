95bef7bdd62a02db04bd41a47a5d8336f864686a
dr-js <dr@dr.run>
2022-12-21 08:05:28 +0000

    CHG: update ci script

--- .github/ci-patch.js
+++ .github/ci-patch.js
@@ -7,8 +7,8 @@ runKit(async (kit) => {
   // kit.padLog('Patch npm cache path') // set cache path to `~/.npm/` for all platform (only win32 for now)
   // kit.RUN_SUDO_NPM([ 'config', '--global', 'set', 'cache', kit.fromHome('.npm/') ])
 
-  kit.padLog('Patch install "@dr-js/core" globally')
-  kit.RUN_SUDO_NPM('install --global @dr-js/core@0.5')
+  // kit.padLog('Patch install "@dr-js/core" globally')
+  // kit.RUN_SUDO_NPM('install --global @dr-js/core@0.5')
 
   // kit.padLog('Patch install "npm@8" globally')
   // kit.RUN_SUDO_NPM('install --global npm@8')
