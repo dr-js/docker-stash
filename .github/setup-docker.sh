5932382b870e63ed196a40f7493b4c411d2a2ca6
dr-js <dr@dr.run>
2026-03-04 01:53:03 +0000

    BREAK: sort repo dev pkg

--- .github/setup-docker.sh
+++ .github/setup-docker.sh
@@ -1,6 +1,6 @@
 set -ex # expect `cwd` at repo root
 
-sudo npm i -g npm@8 @dr-js/core@0.5 @dr-js/dev@0.5
+sudo npm i -g @dr-js/core@0.5 @dr-js/dev@0.5
 dr-dev -eI .github/ci-patch.js
 
 # https://stackoverflow.com/questions/60171603/enable-experimental-features-on-github-workflow-images/60454218#60454218
